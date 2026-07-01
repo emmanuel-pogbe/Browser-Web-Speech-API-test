58a46029fed0e6b20d6af5bd2e551b0dfb2dc2a6
Pogbe Emmanuel <mauyonpogbe@gmail.com>
2026-07-01 12:31:12 +0100

    hello world utterance

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const synth = window.speechSynthesis;
+const utterance = new SpeechSynthesisUtterance("Hello world!");
+utterance.rate = 1.5;
 function buttonFunction() {
     console.log("I've been clicked!");
+    synth.speak(utterance);
 }
+
+
+synth.getVoices();
